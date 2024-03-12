855efb385ff5889b8eace57ecb602012fb15e1bf
mihcihade <163136479+mihcihade@users.noreply.github.com>
2024-03-12 09:40:03 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://static.wikia.nocookie.net/disneyprincesas/images/b/bc/Magic_Golden_Flower.jpg/revision/latest?cb=20180113175457&path-prefix=pt-br style=";
+}
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://disneyplusbrasil.com.br/wp-content/uploads/2022/10/Enrolados-Disney-Plus.jpg";
+}
